d1cdcf099f6bbc44c5432c37e0e71fe634df3f80
Bugra Arslan <bugraarslan5@outlook.com>
2024-09-08 19:16:18 +0300

    updated

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { ref, uploadBytes, getDownloadURL } = require("firebase/storage");
 const { storage } = require("./firebaseConfig");
 const admin = require('./firebaseAdmin');
 const cors = require('cors');
+const nodemailer = require('nodemailer');
 
 const app = express();
 app.use(express.json());
@@ -164,5 +165,37 @@ app.post("/api/upload-image", upload.single("image"), async (req, res) => {
             message: "Image upload failed",
             error: error.message,
         });
+    }
+});
+
+// Contact form API route
+app.post("/api/contact", async (req, res) => {
+    const { name, email, message } = req.body;
+
+    // Nodemailer configuration
+    const transporter = nodemailer.createTransport({
+        host: "smtpout.secureserver.net", // GoDaddy SMTP server
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.EMAIL, // Your corporate email
+            pass: process.env.EMAIL_PASSWORD, // Your email password
+        },
+    });
+
+    // Email options
+    const mailOptions = {
+        from: `"${name}" <${email}>`, // Sender's email
+        to: process.env.EMAIL, // Your corporate email
+        subject: "Web Sitesinden Yeni Email",
+        text: `Name: ${name}\n\nEmail: ${email}\n\n\nMessage:\n${message}`,
+    };
+
+    try {
+        // Send the email
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ success: true, message: "Message sent successfully!" });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Failed to send message", error: error.message });
     }
 });
