256e224ab3697c29819d1a8d39735e12aa852ad8
Bugra Arslan <bugraarslan5@outlook.com>
2025-07-08 12:09:28 +0300

    Refactor: Move server listening logic to the end of app.js for better structure

--- app.js
+++ app.js
@@ -27,3 +27,8 @@ app.use("/api", uploadRoutes);
 app.use("/api", visitorRoutes);
 
 module.exports = app;
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
